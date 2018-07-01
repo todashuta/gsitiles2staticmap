0f32d1f2d3d5b5f140bf93bec6a5d413d175a91c
todashuta <toda.shuta@gmail.com>
2018-07-01 19:38:35 +0900

    Update

--- gsitiles2staicmap.sh
+++ gsitiles2staicmap.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-cachedir="./_cache"
-mkdir -p "$cachedir" || exit 1
-now="./$(date +%Y%m%dT%M%H%S%z)"
-mkdir -p "$now" || exit 1
-
 if [ $# -ne 5 ]; then
 	echo "実行するには5個の引数が必要です" 1>&2
 	exit 1
 fi
+
+tiletype="seamlessphoto"
+src_ext=".jpg"
+#tiletype="std"
+#src_ext=".png"
+
+urlfmt="https://cyberjapandata.gsi.go.jp/xyz/${tiletype}/%s/%s/%s${src_ext}"
+
+cachedir="./_cache/$tiletype"
+mkdir -p "$cachedir" || exit 1
+now="./$(date +%Y%m%dT%H%M%S%z)"
+mkdir -p "$now" || exit 1
 
 zoomlevel=$1
 topleft_x=$2
@@ -18,9 +25,12 @@ bottomright_y=$5
 
 for y in $(seq $topleft_y $bottomright_y); do
 	for x in $(seq $topleft_x $bottomright_x); do
-		echo "$cachedir/${zoomlevel}-${x}-${y}.jpg" >> "$now/tiles.txt"
-		if [ ! -f "$cachedir/${zoomlevel}-${x}-${y}.jpg" ]; then
-			wget "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/${zoomlevel}/${x}/${y}.jpg" -O "$cachedir/${zoomlevel}-${x}-${y}.jpg"
+		out="${cachedir}/${zoomlevel}-${x}-${y}${src_ext}"
+		echo "$out" >> "$now/tiles.txt"
+
+		if [ ! -f "$out" ]; then
+			url=$(printf "$urlfmt" "$zoomlevel" "$x" "$y")
+			wget "$url" -O "$out"
 		fi
 	done
 done
